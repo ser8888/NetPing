5971405b511dbab81e2c7b9dbe7245aa8c0a8a5e
Sergejs Tiselskis <slokas85@gmail.con>
2022-08-10 00:33:31 +0300

    version 1.0

--- NetPing/ViewController.swift
+++ NetPing/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+    @IBOutlet weak var textField: UITextField!
+    
+//        override func viewDidLoad() {
+//        super.viewDidLoad()
+//            let textEntered = "Taylor+Swift"
+//            let urlString = "https://itunes.apple.com/search?term=\(textEntered)&limit=5"
+//        fetchRequest(urlString: urlString)
+//    }
+    
+    
+    @IBAction func sendRequstPressed() {
+ //       print(textField.text ?? "")
+        let urlString = "https://itunes.apple.com/search?term=\(textField.text ?? "")&limit=5"
+    fetchRequest(urlString: urlString)
     }
-
-
+    
+    
+ //   @IBAction func doneButtonPressed() {
+  //      print(textField.text ?? "")
+//        let urlString = "https://itunes.apple.com/search?term=(textField.text)d&limit=5"
+//                fetchRequest(urlString: urlString)
+        
+ //       let inputText = textField.text
+        
+ //       guard let inputText = textField.text, !inputText.isEmpty else {
+//                showAlert(with: "Text field is empty", and: "Please enter your name")
+//                return
+//            }
+//    }
 }
 
+
+private func fetchRequest(urlString: String) {
+    
+    guard let url = URL(string: urlString) else { return }
+    URLSession.shared.dataTask(with: url)  { data, _, error in
+        
+        guard let data = data else {
+            print(error?.localizedDescription ?? "Unknown error")
+            return
+        }
+        do {
+            let tracks = try JSONDecoder().decode(Reply.self, from: data)
+            tracks.results.map { track in
+                print(track.trackName)
+            }
+        } catch let error {
+            print("Failed to decode JSON", error)
+            
+        }
+    }.resume()
+}
